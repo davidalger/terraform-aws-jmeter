f01963faf6d45648a88f8c88c88edd455bb4a768
Michael Boutillier <michael.boutillier@ingenico.com>
2021-04-21 23:44:21 +0200

    feat(module): add variable to allow to pass filter to find the right ami
    Add filter as variable
    Add variable to pass a path to a jmeter.jmx file to upload to the EC2
    Improve doc

--- main.tf
+++ main.tf
@@ -53,9 +53,14 @@ locals {
       content     = filebase64("${path.module}/files/profile.d/boilerplate.sh")
     },
     {
+      path        = "/tmp/jmeter.jmx"
+      permissions = "0644"
+      content     = filebase64(var.jmeter_scenario)
+    },
+    {
       path        = "/usr/local/bin/setup-jmeter.sh"
       permissions = "0755"
-      content     = base64encode(replace(
+      content = base64encode(replace(
         file("${path.module}/files/setup-jmeter.sh"),
         "/JMETER_VERSION=.*/",
         "JMETER_VERSION=${var.jmeter_version}",
@@ -65,12 +70,14 @@ locals {
 }
 
 data "aws_ami" "centos" {
-  owners      = ["125523088429"]
+  owners      = var.instance_owner
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["CentOS 7.9.2009 x86_64"]
+  dynamic "filter" {
+    for_each = var.instance_filter
+    content {
+      name   = lookup(filter.value, "name", "name")
+      values = lookup(filter.value, "values", [])
+    }
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -21,24 +21,49 @@ variable "security_groups" {
 }
 
 variable "instance_type" {
-  type    = string
-  default = "c5.2xlarge"
+  type        = string
+  description = "instance type to deploy"
+  default     = "c5.2xlarge"
+}
+
+variable "instance_owner" {
+  type        = list(string)
+  description = "List of AMI owners to limit search. At least 1 value must be specified. Valid values: an AWS account ID, self (the current account), or an AWS owner alias (e.g. amazon, aws-marketplace, microsoft). Default to centos public account"
+  default     = ["125523088429"]
+}
+variable "instance_filter" {
+  type = list(object({
+    name   = string
+    values = list(string)
+  }))
+  description = "provide a map of filter to search Instance ID (see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami)"
+  default = [{
+    name   = "name"
+    values = ["CentOS 7.9.2009 x86_64"]
+  }]
 }
 
 variable "instance_user" {
-  type    = string
-  default = "jmeter"
+  type        = string
+  default     = "jmeter"
+  description = "service account to run jmeter"
 }
 
 variable "jmeter_version" {
-  type    = string
-  default = "5.1.1"
+  type        = string
+  default     = "5.1.1"
+  description = "jmeter version"
 }
 
 variable "authorized_keys" {
-  type = list(any)
+  type        = list(any)
+  description = "list of ssh pub key allowed to ssh"
 }
-
+variable "jmeter_scenario" {
+  type        = string
+  default     = "./jmeter.jmx"
+  description = "a path to a jmeter jmx file tu upload to the ec2 it will be uploaded to /tmp/jmeter.jmx"
+}
 variable "trusted_ip_ranges" {
   description = "List of IP ranges to whitelist for ICMP and SSH ingress."
   type        = list(string)
